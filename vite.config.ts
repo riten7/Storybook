4e50c2de5ddba85ccd3f1665e78fec2e6d19206e
Riten <riten@NP-RISH-MBP-01.local>
2024-06-19 11:28:04 +0545

    config for styles and build

--- vite.config.ts
+++ vite.config.ts
@@ -2,34 +2,38 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
-// import tsconfigPaths from 'vite-tsconfig-paths'
-import {  peerDependencies } from './package.json';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    // /*  exclude: ['**/*.stories.tsx', '**/*.test.tsx'], */
+    dts({
+      rollupTypes: true,
+      exclude: ['**/storybook-static/**/*'],
+    }), 
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/exports.ts'),
       name: 'components',
-      fileName: (format) => `components.${format}.js`,
+      fileName: 'components',
       formats: ['es', 'cjs', 'umd'],
-    },  
+    }, 
+    //bundler options
+    //externalize react-related imports 
     rollupOptions: {
-      external: Object.keys(peerDependencies),
+      external: ['react', 'react-dom', 'react-jsx/time'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime'
         },
       },
     },
     emptyOutDir: true,
-  },
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-      exclude: ['**/*.stories.tsx', '**/*.test.tsx'],  
-    }),
-  ]
+  }
 })
